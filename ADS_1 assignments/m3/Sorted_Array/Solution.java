8109db0862aa34b6d01d99bfbc858047db6288f9
akshitha111 <akshitha2408@msitprogram.net>
2018-09-26 21:46:42 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS_1 assignments/m3/Sorted_Array/Solution.java
+++ ADS_1 assignments/m3/Sorted_Array/Solution.java
@@ -86,18 +86,21 @@ public final class Solution {
         int s2 = Integer.parseInt(scan.nextLine());
         int[] ar1 = new int[s1];
         int[] ar2 = new int[s2];
+        
         if (s1 > 0) {
             String[] tokens = scan.next().split(",");
             for (int i = 0; i < s1; i++) {
                 ar1[i] = Integer.parseInt(tokens[i]);
             }
         }
+
         if (s2 > 0) {
             String[] tokens1 = scan.next().split(",");
             for (int i = 0; i < s2; i++) {
                 ar2[i] = Integer.parseInt(tokens1[i]);
             }
         }
+
         SortedArray sa = new SortedArray(ar1, ar2);
         int[] a = sa.sort(ar1, ar2);
         System.out.println(Arrays.toString(a).substring(
