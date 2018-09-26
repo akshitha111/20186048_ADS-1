e2b8330b487ca2e43af21aefde3cc2fe9fb82359
akshitha111 <akshitha2408@msitprogram.net>
2018-09-26 17:31:10 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS_1 assignments/m3/3-sum/Solution.java
+++ ADS_1 assignments/m3/3-sum/Solution.java
@@ -21,8 +21,8 @@ class Solution {
 		ThreeSum ts = new ThreeSum();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] arr = new int[n * n];
-		for (int i = 0; i < arr.length; i++) {
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
 		ts.threeSum(arr);
