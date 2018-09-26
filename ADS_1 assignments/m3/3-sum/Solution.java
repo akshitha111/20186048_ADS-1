93c6d3a79ba4218312bb90087e0ed101bcf06a7b
akshitha111 <akshitha2408@msitprogram.net>
2018-09-26 17:43:58 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS_1 assignments/m3/3-sum/Solution.java
+++ ADS_1 assignments/m3/3-sum/Solution.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class ThreeSum {
 	private int count = 0;
-	
+
 	public void threeSum(int[] arr) {
 		for (int i = 0; i < arr.length-2; i++) {
 			if(arr[i] + arr[i+1] + arr[i+2] == 0) {
-				count++;
+				count += 1;
 		}
 		
 			}System.out.println(count);
