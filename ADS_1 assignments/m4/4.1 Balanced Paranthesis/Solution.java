c525af4dea6311d33eaf94b730512789479222a3
akshitha111 <akshitha2408@msitprogram.net>
2018-09-27 17:00:28 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS_1 assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS_1 assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -3,7 +3,44 @@ class BalancedParanthesis {
 	BalancedParanthesis() {
 
 	}
+	public void isBalanced(String[] array) {
+		int countOne = 0;
+		int countTwo = 0;
+		int countThree = 0;
+		int freqOne = 0;
+		int freqTwo = 0;
+		int freqThree = 0;
+		for(int i = 0; i < array.length; i++) {
+			if(array[i] == "(") {
+				countOne += 1;
+			}
+			if(array[i] == "{") {
+				countTwo += 1;
+			}
+			if(array[i] == "(") {
+				countThree += 1;
+			}
+			if(array[i] == "}") {
+				freqOne += 1;
+			}
+			if(array[i] == "]") {
+				freqTwo += 1;
+			}
+			if(array[i] == ")") {
+				freqThree += 1;
+			}
+		}
+		if(countOne == freqOne && countTwo == freqTwo && countThree == freqThree) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+
+
+
+	}
 }
+
 
 public class Solution {
 	Solution() {
@@ -20,7 +57,8 @@ public class Solution {
 		/*for (int j = 0; j < input; j++) {
 			System.out.println(bp.balanced(bracket[j]));
 		}*/
-		System.out.println(Arrays.toString(bracket));
+		//System.out.println(Arrays.toString(bracket));
+		bp.isBalanced(bracket);
 
 	}
 }
