10c4e2457b400c66b912c18dfaead1b6e199a40f
akshitha111 <akshitha2408@msitprogram.net>
2018-09-29 12:48:51 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS_1 assignments/m6/Assignment-1/Solution.java
+++ ADS_1 assignments/m6/Assignment-1/Solution.java
@@ -1,8 +1,26 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Iterator;
+/**
+ * Class for add large numbers.
+ */
 
-class AddLargeNumbers {
-  
-    public static LinkedList numberToDigits(String number) {
+final class AddLargeNumbers {
+    /**
+     * Constructs the object.
+     */
+    private AddLargeNumbers() {
+
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      number  The number
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static LinkedList numberToDigits(final String number) {
             LinkedList one = new LinkedList(Arrays.asList(number));
             Iterator iterator = one.iterator();
             while (iterator.hasNext()) {
@@ -13,43 +31,53 @@ class AddLargeNumbers {
     /*public static String digitsToNumber(LinkedList list) {
         return;
     }*/
-
-    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-        
+    /**
+     * Adds large numbers.
+     *
+     * @param      list1  The list 1
+     * @param      list2  The list 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static LinkedList addLargeNumbers(final LinkedList list1,
+     final LinkedList list2) {
             LinkedList a = list1;
             LinkedList b = list2;
             LinkedList c;
             int sum = 0;
             int carry;
-            for(int i = list1.size(); i > 0; i--) {
-                for(int j = list2.size(); j > 0; j--) {
+            for (int i = list1.size(); i > 0; i--) {
+                for (int j = list2.size(); j > 0; j--) {
                    sum += (j + i);
-                   if(sum > 10) {
-                    sum = sum % 10;
-                        
-                    }//c = sum;
-                     
+                   if (sum > 2 * 2 * 2 + 2) {
+                    sum = sum % 2 * 2 * 2 + 2;
+                    } //c = sum;
                    }
 
-                }return b;
+                } return b;
             }
-
-            
-        
-
 }
-    
+/**
+ * { item_description }.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-
-
-
-public class Solution {
-    public static void main(String[] args) {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
+        switch (input) {
             case "numberToDigits":
                 LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
                 LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
@@ -60,10 +88,16 @@ public class Solution {
             case "addLargeNumbers":
                 pDigits = AddLargeNumbers.numberToDigits(p);
                 qDigits = AddLargeNumbers.numberToDigits(q);
-                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+                LinkedList result = AddLargeNumbers.addLargeNumbers(
+                    pDigits, qDigits);
                 //System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
+
+            default:
                 break;
         }
     }
-    
 }
+
+
+
