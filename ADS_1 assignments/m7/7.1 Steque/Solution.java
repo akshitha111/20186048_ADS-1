f50e30dfa3757f0aec96bf46b3780a24654dcc84
akshitha111 <akshitha2408@msitprogram.net>
2018-10-01 18:20:19 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS_1 assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+class Node {
+	private int value;
+	private Node friend;
+
+	Node(int v, Node f) {
+		this.value = v;
+		this.friend= f;
+	}
+
+	public void setFriend(final Node f) {
+		this.friend = f;
+	}
+
+	public void setValue(int v) {
+		this.value = v;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public Node getFriend() {
+		return this.friend;
+	}
+
+	public String toString() {
+		return value + "";
+	}
+}
+
+class Steque {
+private Node head, tail;
+private int size;
+//time complexity of steque is 1
+	Steque() {
+	head = tail = null;
+	size = 0;
+	}
+
+//inserts element at first of list.
+//time complexity for push() is 1.
+public void push(int value) {
+	Node old = head;
+	head = new Node(value, old);
+	if(old==null)
+		tail = head;
+		size++;
+}
+
+//remove element at beginning of the list
+//time complexity for pop() is 1.
+public void pop() {
+	if(head != null && tail !=null){
+		if(head.getFriend() == null && tail.getFriend() == null) {
+			tail = head = null;
+			size--;
+	}
+	else if(head!=null) {
+		head = head.getFriend();
+		size--;
+	}
+}
+}
+
+//inserts element at end of list.
+//time complexity is 1.
+public void enqueue(int value) {
+	if(tail != null) {
+		Node oldtail = tail;
+		tail = new Node(value, null);
+		oldtail.setFriend(tail);
+	} else if(tail == null && head == null) {
+		head = tail = null;
+		}
+	}
+
+//timecomplexity for tostring() is n
+//n is the size of the list
+public String toString() {
+	StringBuffer sb = new StringBuffer("");
+	if(size == 0) {
+	sb.append("Steque is empty.");
+	return sb.toString();
+	} else {
+	Node temp = head;
+	while (temp != null) {
+		sb.append(temp + ", ");
+		temp = temp.getFriend();
+	}
+	return sb.toString().substring(0, sb.length() - 2);
+}
+}
+}
+class Solution {
+
+	private Solution() {
+		//unused constructor.
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		while(n > 0) {
+			Steque st = new Steque();
+				while(sc.hasNext()) {
+					String[] tokens = sc.nextLine().split(" ");
+				switch (tokens[0]) {
+					case "push":
+						st.push(Integer.parseInt(tokens[1]));
+						System.out.println(st);
+						break;
+					case "enqueue":
+						st.enqueue(Integer.parseInt(tokens[1]));
+						System.out.println(st);
+						break;
+					case "pop":
+						st.pop();
+						System.out.println(st);
+						break;
+					default:
+						st = new Steque();
+						System.out.println();
+						break;
+				}
+			}n--;
+		}
+	}
+}
