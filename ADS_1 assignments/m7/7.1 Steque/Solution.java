7813293fbeb9dcd86c21479e375648c5b134aeda
akshitha111 <akshitha2408@msitprogram.net>
2018-10-01 18:50:57 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- ADS_1 assignments/m7/7.1 Steque/Solution.java
+++ ADS_1 assignments/m7/7.1 Steque/Solution.java
@@ -1,130 +1,201 @@
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
-	private int value;
-	private Node friend;
-
-	Node(int v, Node f) {
-		this.value = v;
-		this.friend= f;
-	}
-
-	public void setFriend(final Node f) {
-		this.friend = f;
-	}
-
-	public void setValue(int v) {
-		this.value = v;
-	}
-
-	public int getValue() {
-		return this.value;
-	}
-
-	public Node getFriend() {
-		return this.friend;
-	}
-
-	public String toString() {
-		return value + "";
-	}
+    /**
+     * { this is an int variable for value }.
+     */
+    private int value;
+    /**
+     * { this is a node variable for friend }.
+     */
+    private Node friend;
+    /**
+     * this is a constructor.
+     *
+     * @param      v     { int value }.
+     * @param      f     { node friend }.
+     */
+    Node(final int v, final Node f) {
+        this.value = v;
+        this.friend = f;
+    }
+    /**
+     * Sets the friend.
+     *
+     * @param      f     { node friend }.
+     */
+    public void setFriend(final Node f) {
+        this.friend = f;
+    }
+    /**
+     * Sets the value.
+     *
+     * @param      v     { int value }.
+     */
+    public void setValue(final int v) {
+        this.value = v;
+    }
+    /**
+     * Gets the value.
+     *
+     * @return     The value.
+     */
+    public int getValue() {
+        return this.value;
+    }
+    /**
+     * Gets the friend.
+     *
+     * @return     The friend.
+     */
+    public Node getFriend() {
+        return this.friend;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return value + "";
+    }
 }
-
+/**
+ * Class for steque.
+ */
 class Steque {
+    /**
+     * { this is a node variable for head and tail }.
+     */
 private Node head, tail;
+/**
+ * { this is an int variable for size }.
+ */
 private int size;
-//time complexity of steque is 1
-	Steque() {
-	head = tail = null;
-	size = 0;
-	}
+/**
+ * time complexity of steque is 1.
+ */
+    Steque() {
+    tail = null;
+    head = tail;
+    size = 0;
+    }
 
-//inserts element at first of list.
-//time complexity for push() is 1.
-public void push(int value) {
-	Node old = head;
-	head = new Node(value, old);
-	if(old==null)
-		tail = head;
-		size++;
+
+/**
+ * inserts element at first of list.
+ * time complexity for push() is 1.
+ *
+ * @param      value  The value
+ */
+public void push(final int value) {
+    Node old = head;
+    head = new Node(value, old);
+    if (old == null) {
+        tail = head;
+    }
+        size++;
 }
 
-//remove element at beginning of the list
-//time complexity for pop() is 1.
+/**
+ * remove element at beginning of the list
+   time complexity for pop() is 1.
+ */
 public void pop() {
-	if(head != null && tail !=null){
-		if(head.getFriend() == null && tail.getFriend() == null) {
-			tail = head = null;
-			size--;
-	}
-	else if(head!=null) {
-		head = head.getFriend();
-		size--;
-	}
+    if (head != null && tail != null) {
+        if (head.getFriend() == null && tail.getFriend() == null) {
+            head = null;
+            tail = head;
+            size--;
+    }  else if (head != null) {
+        head = head.getFriend();
+        size--;
+    }
 }
 }
-
-//inserts element at end of list.
-//time complexity is 1.
-public void enqueue(int value) {
-	if (tail == null && head == null) {
-			head = tail = new Node(value, null);
-		} else if (tail != null) {
-			Node oldtail = tail;
-			tail = new Node(value, null);
-			oldtail.setFriend(tail);
-		}
-		size++;
-	}
-
-//timecomplexity for tostring() is n
-//n is the size of the list
+/**
+ * inserts element at end of list.
+   time complexity is 1.
+ *
+ * @param      value  int value
+ */
+public void enqueue(final int value) {
+    if (tail == null && head == null) {
+            head =  new Node(value, null);
+            tail =  new Node(value, null);
+        } else if (tail != null) {
+            Node oldtail = tail;
+            tail = new Node(value, null);
+            oldtail.setFriend(tail);
+        }
+        size++;
+    }
+/**
+ * timecomplexity for tostring() is n
+   n is the size of the list.
+ *
+ * @return     String representation of the object.
+ */
 public String toString() {
-	StringBuffer sb = new StringBuffer("");
-	if(size == 0) {
-	sb.append("Steque is empty.");
-	return sb.toString();
-	} else {
-	Node temp = head;
-	while (temp != null) {
-		sb.append(temp + ", ");
-		temp = temp.getFriend();
-	}
-	
-}return sb.toString().substring(0, sb.length() - 2);
+    StringBuffer sb = new StringBuffer("");
+    if (size == 0) {
+    sb.append("Steque is empty.");
+    return sb.toString();
+    } else {
+    Node temp = head;
+    while (temp != null) {
+        sb.append(temp + ", ");
+        temp = temp.getFriend();
+    }
+}
+return sb.toString().substring(0, sb.length() - 2);
 }
 }
-class Solution {
 
-	private Solution() {
-		//unused constructor.
+/**
+ * Class for solution.
+ */
+final class Solution {
+/**
+ * Constructs the object.
+ */
+    private Solution() {
+        //unused constructor.
 
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		while(n > 0) {
-			Steque st = new Steque();
-				while(sc.hasNext()) {
-					String[] tokens = sc.nextLine().split(" ");
-				switch (tokens[0]) {
-					case "push":
-						st.push(Integer.parseInt(tokens[1]));
-						System.out.println(st);
-						break;
-					case "enqueue":
-						st.enqueue(Integer.parseInt(tokens[1]));
-						System.out.println(st);
-						break;
-					case "pop":
-						st.pop();
-						System.out.println(st);
-						break;
-					default:
-						st = new Steque();
-						System.out.println();
-						break;
-				}
-			}n--;
-		}
-	}
+    }
+    /**
+     * { Client program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        while (n > 0) {
+            Steque st = new Steque();
+                while (sc.hasNext()) {
+                    String[] tokens = sc.nextLine().split(" ");
+                switch (tokens[0]) {
+                    case "push":
+                        st.push(Integer.parseInt(tokens[1]));
+                        System.out.println(st);
+                        break;
+                    case "enqueue":
+                        st.enqueue(Integer.parseInt(tokens[1]));
+                        System.out.println(st);
+                        break;
+                    case "pop":
+                        st.pop();
+                        System.out.println(st);
+                        break;
+                    default:
+                        st = new Steque();
+                        System.out.println();
+                        break;
+                }
+            } n--;
+        }
+    }
 }
