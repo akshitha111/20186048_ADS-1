57d6950f9d7ce925ab3651c96e3e4bf4524a93ec
akshitha111 <akshitha2408@msitprogram.net>
2018-10-02 17:06:21 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS_1 assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+class Ranking {
+
+	private String country;
+	private int wins;
+	private int losses;
+	private int draws;
+
+	Ranking(String c, int w, int l, int d) {
+		this.country = c;
+		this.wins = w;
+		this.losses = l;
+		this.draws = d;
+	}
+
+	public String getCountry() {
+		return this.country;
+	}
+
+	public int getWins() {
+		return this.wins;
+	}
+
+	public int getLosses() {
+		return this.losses;
+
+	}
+
+	public int getDraws() {
+		return this.draws;
+	}
+
+	public int compareTo(Ranking that) {
+		if (this.getWins() < that.getWins()) {
+			return 1;
+		} else if (this.getWins() > that.getWins()) {
+			return -1;
+		} else {
+		if (this.getLosses() < that.getLosses()) {
+			return -1;
+		} else if (this.getLosses() > that.getLosses()) {
+			return 1;
+		} else {
+			if (this.getDraws() < that.getDraws()) {
+				return 1;
+			} else if (this.getDraws() > that.getDraws()) {
+				return -1;
+			}
+		}
+		} return 0;
+	}
+}
+
+class Sorting {
+
+	private Ranking[] ranking;
+	private int size;
+
+	Sorting() {
+		ranking = new Ranking[100];
+		size = 0;
+	}
+
+	public void add(Ranking r) {
+		ranking[size++] = r;
+	}
+
+	public void sort() {
+		for (int i = 1; i < size; i++) {
+			Ranking rank = ranking[i];
+			int j = i - 1;
+			int count = rank.compareTo(ranking[j]);
+			while (j >= 0 && count == -1) {
+				ranking[j + 1] = ranking[j];
+				j--;
+				if (j >= 0) {
+					count = rank.compareTo(ranking[j]);
+				}
+			} ranking[j+1] = rank;
+		}
+	}
+
+}
+
+
+
+public class Solution {
+
+	private Solution() {
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			String[] tokens = sc.nextLine().split(",");
+			Ranking r = new Ranking(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
+
+		}
+	}
+}
