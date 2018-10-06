83957575d809d9942576267d4d645227b653fd7c
akshitha111 <akshitha2408@msitprogram.net>
2018-10-06 13:06:35 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS_1 assignments/m12/Assignment-1/Solution.java
+++ ADS_1 assignments/m12/Assignment-1/Solution.java
@@ -26,42 +26,7 @@ class Student {
 		this.reservationCatergory = rc;
 	}
 
-	/*public void setName(final String name) {
-		this.name = name;
-	}
-
-	public void setDate(final int date) {
-		this.date = date;
-	} 
-
-	public void setMonth(final int month) {
-		this.month = month;
-	} 
-
-	public void setYear(final int year) {
-		this.year = year;
-	} 
-
-	public void setSubjectOne(final int subjectOne) {
-		this.subjectOne = subjectOne;
-	}
-
-	public void setSubjectTwo(final int subjectTwo) {
-		this.subjectTwo = subjectTwo;
-	}
-
-	public void setSubjectThree(final int subjectThree) {
-		this.subjectThree = subjectThree;
-	}
-
-	public void setTotal(final int total) {
-		this.total = total;
-	}
-
-	public void setReservationCat(final String reservationCatergory) {
-		this.reservationCatergory = reservationCatergory;
-	}*/
-
+	
 	public String getName() {
 		return this.name;
 	}
@@ -180,36 +145,23 @@ class Sorting {
             s += student[i].getName() + "," + student[i].getTotal() + "," + student[i].getReservationCat() + "\n";
 
         }
-        System.out.println();
+        //System.out.println();
         return s.substring(0, s.length() - 1);
     }
 
 }
 
-/*class Date implements Comparable<Date> {
-	private final int date, month, year;
+	
+/*class Reseravtion {
+	Reservation() {
+		String[] seats = new String[4];
+		for(int i = 0; i < 4; i++) {
+			seats.add
+		}
+		
 
-	Date() {
-
-	}
-
-	public Date(int d, int m, int y) {
-		this.date = d;
-		this.month = m;
-		this.year = y;
-	}
-
-	public int compareTo(Date that) {
-		if (this.year < that.year) return -1;
-		if (this.year > that.year) return 1;
-		if (this.month < that.month) return -1;
-		if (this.month > that.month) return 1;
-		if (this.date < that.date) return -1;
-		if (this.date > that.date) return 1;
-		return 0;
 	}
 }*/
-	
 
 
 public final class Solution {
@@ -235,6 +187,9 @@ public final class Solution {
 		s.add(new Student((tokens[0]), Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), Integer.parseInt(tokens[6]), Integer.parseInt(tokens[7]), (tokens[8])));
 	} s.sort();
 	System.out.println(s);
+	for(int i = 1; i < 4; i++) {
+		System.out.println(s);
+	}
 }
 }
 	
