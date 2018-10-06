c6dbe07b532e2f5f1be3208668eb1ba72fffb7ed
akshitha111 <akshitha2408@msitprogram.net>
2018-10-06 12:34:19 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS_1 assignments/m12/Assignment-1/Solution.java
+++ ADS_1 assignments/m12/Assignment-1/Solution.java
@@ -222,8 +222,9 @@ public final class Solution {
 	while(sc.hasNext()) {
 		String[] tokens = sc.nextLine().replace("-", ",").split(",");
 		//String[] check = tokens[1].split("-"); 
-		System.out.println(tokens);
-		s.add(new Student(tokens[1], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), Integer.parseInt(tokens[6]), Integer.parseInt(tokens[7]), tokens[8]));
+		//System.out.println(tokens.toString());
+		//Student[] = new Student(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), Integer.parseInt(tokens[6]), Integer.parseInt(tokens[7]), tokens[8]);
+		s.add(new Student((tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), Integer.parseInt(tokens[6]), Integer.parseInt(tokens[7]), Integer.parseInt(tokens[8]), (tokens[9])));
 	} s.sort();
 	System.out.println(s);
 }
