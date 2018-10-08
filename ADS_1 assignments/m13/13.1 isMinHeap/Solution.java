7cb5742b5447b99ec8550924774582d93b304bea
akshitha111 <akshitha2408@msitprogram.net>
2018-10-08 17:52:14 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS_1 assignments/m13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class MinHeap<E extends Comparable<E>>  {
+	private E[] arr;
+	public MinHeap(E[] str) {
+		this.arr = str;
+	}
+	public boolean isMinHeap() {
+		for(int i = 0; i < arr.length; i++) {
+			if (!less(arr[i], arr[i + 1])) {
+				return false;
+			}
+		}return true;
+	}
+		public boolean less(E i, E j) {
+		boolean a =  i.compareTo(j) <= 0;
+		return a;
+		}
+	}
+
+public class Solution {
+	private Solution() {
+
+	} public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		switch (s) {
+		case "String":
+		int n = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < n; i++) {
+			String[] line = sc.nextLine().split(" ");
+			MinHeap<String> mh = new MinHeap<String>(line);
+				System.out.println(mh.isMinHeap());
+		}break;
+		case "Integer":
+		n = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < n; i++) {
+			String[] line = sc.nextLine().split(" ");
+			System.out.println(line);
+		}break;
+		case "Double":
+		n = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < n; i++) {
+			String[] line = sc.nextLine().split(" ");
+		}break;
+		case "Float":
+		n = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < n; i++) {
+			String[] line = sc.nextLine().split(" ");
+		}break;
+	}
+	}
+}
