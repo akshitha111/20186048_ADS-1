6dab236817a9a21c9d3be8a6aba7660a316dc8c1
akshitha111 <akshitha2408@msitprogram.net>
2018-10-13 11:57:50 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS_1 assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS_1 assignments/m18/Week-3_Assignment-1/Solution.java
@@ -52,8 +52,8 @@ public final class Solution {
 			Stock s = minpq.delMin();
 			 System.out.print(s.getName() + " ");
 			 System.out.println(s.getPercent());
-			 System.out.println();
-			}
+			 
+			}System.out.println();
 			for(int i = 0; i < 5; i++) {
 			Stock s = maxpq.delMax();
 			 System.out.print(s.getName() + " ");
