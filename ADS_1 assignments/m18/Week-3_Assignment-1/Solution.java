9ba738f0d0b38b72eda5cee17c0acd1e42c742fa
akshitha111 <akshitha2408@msitprogram.net>
2018-10-13 11:11:26 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS_1 assignments/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class Stock implements Comparable<Stock> {
+	private String name;
+	private float percent;
+	Stock(String n, float p) {
+		this.name = n;
+		this.percent = p;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public float getPercent() {
+		return this.percent;
+	}
+	public int compareTo(final Stock that) {
+        if (this.percent < that.percent) {
+            return -1;
+        }
+        if (this.percent > that.percent) {
+            return +1;
+        }
+        return 0;
+    }
+}
+public final class Solution {
+	private Solution() {
+
+	}
+	public static void main(String[] args) {
+		/*Stock s = new Stock(int );*/
+		MinPQ<Stock> minpq = new MinPQ<Stock>();
+		MaxPQ<Stock> maxpq = new MaxPQ<Stock>();
+		Scanner sc = new Scanner(System.in);
+		int noOfStocks = Integer.parseInt(sc.nextLine());
+		//System.out.println(noOfStocks);
+		for(int i = 0; i < 6 * noOfStocks; i++) {
+			String[] tokens = sc.nextLine().split(",");
+			//System.out.println(tokens);
+			minpq.insert(new Stock(tokens[0], Float.parseFloat(tokens[1])));
+			maxpq.insert(new Stock(tokens[0], Float.parseFloat(tokens[1])));
+		}
+		while(!minpq.isEmpty()) {
+			 Stock s = minpq.delMin();
+			 System.out.println(s.getPercent());
+		}
+		//System.out.println(getPercent());
+		int noOfQueries = Integer.parseInt(sc.nextLine());
+		//System.out.println(noOfQueries);
+		if(noOfQueries > 0) {
+			for(int i = 0; i < noOfQueries; i++) {
+				String[] check = sc.nextLine().split(",");
+				//System.out.println(check[0]);
+			}
+		}
+	}
+}
