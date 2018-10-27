44b11dc451dab26c618265e247abfbe1718f56c1
akshitha111 <akshitha2408@msitprogram.net>
2018-10-27 11:41:33 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS_1 assignments/m24/2/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+class Student {
+	private int rollNumber;
+	private String name;
+	private double marks;
+	Student(int r, String n, double m) {
+		this.rollNumber = r;
+		this.name = n;
+		this.marks= m;
+	}
+	public int getRollNumber() {
+		return this.rollNumber;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public double getMarks() {
+		return this.marks;
+	}
+
+	public int compareTo(final Object object) {
+        Student that = (Student) object;
+        return this.name.compareTo(that.name);
+    }
+}
+
+
+
+class Solution {
+	Solution() {
+	}
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			BinarySearchST bstobj = new BinarySearchST();
+		int a = Integer.parseInt(sc.nextLine());
+		while(a > 0) {
+			String[] check = sc.nextLine().split(",");
+			Student s = (new Student(Integer.parseInt(check[0]), (check[1]), Double.parseDouble(check[2])));
+
+			a--;
+		}
+		int b = Integer.parseInt(sc.nextLine());
+		while(b > 0) {
+			String[] check = sc.nextLine().split(" ");
+			switch(check[0]) {
+				case "BE":
+					bstobj.keys();
+			}
+			b--;
+		}
+
+		}
+	}
