df005db369e5eed33380cfe4486dca60850c776a
akshitha111 <akshitha2408@msitprogram.net>
2018-10-28 19:42:27 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS_1 assignments/m24/1/Solution.java
+++ ADS_1 assignments/m24/1/Solution.java
@@ -1,37 +1,45 @@
 import java.util.Scanner;
-class Solution {
-    Solution() {
-
-    }public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * unused constructor.
+     */
+    private Solution() {
+    }
+    /**
+     * solution for main method.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        SeparateChainingHashST<Integer, String> hash1 = new SeparateChainingHashST();
-        SeparateChainingHashST<Integer, Double> hash2 = new SeparateChainingHashST();
-        int n = Integer.parseInt(sc.nextLine());
-        while(n-- != 0) {
-            String[] tokens = sc.nextLine().split(",");
-            hash1.put(Integer.parseInt(tokens[0]), tokens[1]);
-            hash2.put(Integer.parseInt(tokens[0]), Double.parseDouble(tokens[2]));
+        SeparateChainingHashST<Integer, String> hash1 =
+        new SeparateChainingHashST();
+        SeparateChainingHashST<Integer, Double> hash2 =
+        new SeparateChainingHashST();
+        int input = Integer.parseInt(sc.nextLine());
+        while (input-- != 0) {
+            String[] inp = sc.nextLine().split(",");
+            hash1.put(Integer.parseInt(inp[0]), inp[1]);
+            hash2.put(Integer.parseInt(inp[0]), Double.parseDouble(inp[2]));
         }
-        int a = Integer.parseInt(sc.nextLine());
-        while(a-- != 0) {
-            String[] check = sc.nextLine().split(" ");
-            if(check[0].equals("get") && Integer.parseInt(check[2]) == 1) {
-                if(hash1.get(Integer.parseInt(check[1])) == (null)) {
-                System.out.println("Student does not exist...");
+        int m = Integer.parseInt(sc.nextLine());
+        while (m-- != 0) {
+            String[] tokens = sc.nextLine().split(" ");
+            if (tokens[0].equals("get") && tokens[2].equals("1")) {
+                if (hash1.get(Integer.parseInt(tokens[1])) == null) {
+                    System.out.println("Student doesn't exists...");
+                } else {
+                    System.out.println(hash1.get(Integer.parseInt(tokens[1])));
+                }
+            } else if (tokens[0].equals("get") && tokens[2].equals("2")) {
+                if (hash2.get(Integer.parseInt(tokens[1])) == null) {
+                    System.out.println("Student doesn't exists...");
+                } else {
+                    System.out.println(hash2.get(Integer.parseInt(tokens[1])));
+                }
             }
-            else {
-                System.out.println(hash1.get(Integer.parseInt(check[1])));                
-            }
-            }
-            if(check[0].equals("get") && Integer.parseInt(check[2]) == 1) {
-                if(hash2.get(Integer.parseInt(check[1])) == (null)) {
-                System.out.println("Student does not exist...");
-            }
-            else{
-                System.out.println(hash2.get(Integer.parseInt(check[2])));
-            }
-
         }
     }
-}
 }
